d21ae1e907c626ecc4c1b0dc15bdf2d484fc8994
Christopher Moje <42236736+chrismoje@users.noreply.github.com>
2022-12-26 00:23:29 +0800

    My First New Assignent

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -5,77 +5,59 @@
 // this formula applies to people age 20 and above:
 
 var bmi_index = 0; //initialize the global variable to zero
-let bmi_interpret = " "
+
 
 
 
 function CalculateBMI_metric (height_m,weight_kg){
+
 let bmi_index = weight_kg / Math.pow(height_m,2)
 
 
 if ((bmi_index <= 15)) {
-    bmi_interpret = "VERY severely underweight"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+   let bmi_interpret = 'Very severely underweight';
+    return bmi_interpret;
+  
 
 
 } else if ((bmi_index >15 ) && (bmi_index < 16)) {
-    bmi_interpret = "Severely underweight"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+   let bmi_interpret = 'Severely underweight';
+   return bmi_interpret;
+  
 
 } else if ((bmi_index => 16) && (bmi_index < 18.5)) {
-    bmi_interpret = "Underweight"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+    let bmi_interpret = 'Underweight';
+   return bmi_interpret;
+  
 
 
 } else if ((bmi_index => 18.5) && (bmi_index < 25)) {
-    bmi_interpret = "Normal"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+    let bmi_interpret = 'Normal';
+   return bmi_interpret;
+    
 
 } else if ((bmi_index => 25) && (bmi_index < 30)) {
-    bmi_interpret = "Overweight"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+    let bmi_interpret = 'Overweight';
+    return bmi_interpret;
  
 } else if ((bmi_index => 30) && (bmi_index < 35)) {
-    bmi_interpret = "Moderately Obese"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+    let bmi_interpret = 'Moderately Obese';
+    return bmi_interpret;
 
 } else if ((bmi_index => 35) && (bmi_index < 40)) {
-    bmi_interpret = "Severely Obese"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
+    let bmi_interpret = 'Severely Obese';
+    return bmi_interpret;
  
 } else if (bmi_index=> 40) {
-    bmi_interpret = "Very Severely Obese"
-    console.log('BMI Index = ' + bmi_index );
-    bmi_analyze(bmi_interpret);
-    //return bmi_interpret;
-    
- 
-}
-return bmi_index;
+    let bmi_interpret = 'Very Severely Obese'
+    return bmi_interpret;
+   // console.log(bmi_interpret);
+};    
+} 
 
-};
-
-function bmi_analyze(bmi_interpret){
-    console.log('BMI Analysis: '+ bmi_interpret);
-};
     
 
-
-CalculateBMI_metric(1.75,80);
+//CalculateBMI_metric(1.75,80);
 
 module.exports = CalculateBMI_metric
 //module.exports = bmi_interpret
